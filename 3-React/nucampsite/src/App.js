283ea1ba185c49a18ef594b99eff349911589881
Dustin Piercy <dustyray75@gmail.com>
2022-11-04 09:34:02 -0700

    CampsiteDetailPage with useParams

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,10 +1,11 @@
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import './App.css';
 
 
@@ -14,9 +15,13 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-          <Route path='/' element={<HomePage />}/>
-          <Route path='contact' element={<ContactPage />}/>
-          <Route path='directory' element={<CampsitesDirectoryPage />}/>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route 
+          path='directory/:campsiteId'
+          element={<CampsiteDetailPage />}
+        />
       </Routes>
       <Footer />
     </div>
