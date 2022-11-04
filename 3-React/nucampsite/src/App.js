efa9bedc7d8ff09e08c57319dbbc69a3d91f2756
Dustin Piercy <dustyray75@gmail.com>
2022-11-04 16:33:22 -0700

    Week Two Workshop First Attempt

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
 import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -18,6 +19,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='contact' element={<ContactPage />} />
         <Route path='directory' element={<CampsitesDirectoryPage />} />
+        <Route path='about' element={<AboutPage />} />
         <Route 
           path='directory/:campsiteId'
           element={<CampsiteDetailPage />}
